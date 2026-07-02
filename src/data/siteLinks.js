2f6d870281902ebb77bf16c6de5ee6c5c26bbcc4
rishikagade <rishikagade01@gmail.com>
2026-07-02 16:41:40 -0400

    Update resume asset

--- src/data/siteLinks.js
+++ src/data/siteLinks.js
@@ -1,7 +1,5 @@
-import resumeUrl from '../../Resume/Resume_Rishika_Gade.pdf';
-
 export const EMAIL = 'rishikagade01@gmail.com';
 export const PHONE = '+1 (636) 368-8679';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/rishika-gade';
 export const GITHUB_URL = 'https://github.com/rishikagade';
-export const RESUME_URL = resumeUrl;
+export const RESUME_URL = `${import.meta.env.BASE_URL}assets/Resume_RishikaGade.pdf`;
