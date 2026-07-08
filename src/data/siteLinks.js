f9c18b89aa72b0902a187bd8072bd51567e28406
rishikagade <rishikagade01@gmail.com>
2026-07-08 18:55:12 -0400

    Point resume link to new PDF

--- src/data/siteLinks.js
+++ src/data/siteLinks.js
@@ -2,4 +2,4 @@ export const EMAIL = 'rishikagade01@gmail.com';
 export const PHONE = '+1 (636) 368-8679';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/rishika-gade';
 export const GITHUB_URL = 'https://github.com/rishikagade';
-export const RESUME_URL = `${import.meta.env.BASE_URL}assets/Resume_RishikaGade.pdf`;
+export const RESUME_URL = `${import.meta.env.BASE_URL}assets/Rishika_Gade.pdf`;
